9a023a36f104e0db2b3b0daf89ac0720f1e027f9
Iago <iagoddc@gmail.com>
2026-03-06 00:08:01 -0300

    pequenos ajustes na navbar e na logica de funcionamento

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 const bar = document.getElementById('bar');
 const close = document.getElementById('close');
 const nav = document.getElementById('navbar');
+const mobile = document.getElementById('mobile');
 
 if (bar) {
     bar.addEventListener('click', () => {
         nav.classList.add('active');
+        bar.style.display = "none";
+        mobile.style.display = "none";
     })
 }
 
 if (close) {
     close.addEventListener('click', () => {
         nav.classList.remove('active');
+        bar.style.display = "block";
+        mobile.style.display = "flex";
     })
 }
